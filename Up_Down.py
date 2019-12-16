9d0a25e172a9a97c1d7dc266e43cc221c34acf29
dydtn8653 <dydtn8653@gmail.com>
2019-12-16 16:26:46 +0900

    Up_Down Game

---
+++ Up_Down.py
@@ -0,0 +1,23 @@
+import random
+
+# random "UP DOWN" game
+
+rn = random.randrange(1, 101, 1)
+num = -1
+
+t_cnt = 0 # Try
+
+print("1~100 Number Up & Down Game start !!!")
+print("---------------------------")
+while ( rn != num ):
+
+    num = int(input("Please enter a number between 1 ~ 100. : "))
+
+    if (num > rn):
+        print("Down")
+    elif (num < rn):
+        print("Up")
+
+    t_cnt += 1
+print("---------------------------")
+print("You got the right answer in", t_cnt, "times.")
