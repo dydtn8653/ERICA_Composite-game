05cd485b063049777f40533019691623953c1d8f
dydtn8653 <55014664+dydtn8653@users.noreply.github.com>
2019-12-19 16:06:24 +0900

    Update Number_BaseBall.py

--- Number_BaseBall.py
+++ Number_BaseBall.py
@@ -1,12 +1,4 @@
 import random
-game=0
-out=0
-temp=[]
-print('===== Number baseball  =====')
-print('====== OSS PROJECT ======')
-print()
-
-myNum=random.sample(range(0,9),3)
 
 # Check Function
 def Check(y):
@@ -35,44 +27,54 @@ def StrikeBall(x):
 
 
 # Main
-while True:
-    
-    if game==9:
-        print('> game over')
-        print('The answer was {}{}{}'.format(myNum[0],myNum[1],myNum[2]))
-        break
-    elif out==3:
-        print('> game over')
-        print('The answer was {}{}{}'.format(myNum[0],myNum[1],myNum[2]))
-        break
-    else:
-        while True:
-            guess=input('> {} guess : '.format(game+1))
+def game() :
+    game=0
+    out=0
+    temp=[]
+    print('===== Number baseball  =====')
+    print('====== OSS PROJECT ======')
+    print()
 
-            if Check(guess)==1:
-                print('Please enter a three-digit number.')
-                print()
-            elif Check(guess)==2:
-                print('Please enter it so that it cannot be duplicated.')
-                print()
-            else:
-                break
+    myNum=random.sample(range(0,9),3)
 
-        for i in range(3):
-            temp.append(int(guess[i]))
+    while True:
 
-        StrikeBall(temp)
-        temp=[]
-        
-        if strike==ball==0:
-            print('{} out'.format(out+1))
-            print()
-            out+=1
-
+        if game==9:
+            print('> game over')
+            print('The answer was {}{}{}'.format(myNum[0],myNum[1],myNum[2]))
+            break
+        elif out==3:
+            print('> game over')
+            print('The answer was {}{}{}'.format(myNum[0],myNum[1],myNum[2]))
+            break
         else:
-            print('{} strike, {} ball'.format(strike,ball-strike))
-            print()
-            if strike==3:
-                print('> You win')
-                break
-        game+=1
+            while True:
+                guess=input('> {} guess : '.format(game+1))
+
+                if Check(guess)==1:
+                    print('Please enter a three-digit number.')
+                    print()
+                elif Check(guess)==2:
+                    print('Please enter it so that it cannot be duplicated.')
+                    print()
+                else:
+                    break
+
+            for i in range(3):
+                temp.append(int(guess[i]))
+
+            StrikeBall(temp)
+            temp=[]
+
+            if strike==ball==0:
+                print('{} out'.format(out+1))
+                print()
+                out+=1
+
+            else:
+                print('{} strike, {} ball'.format(strike,ball-strike))
+                print()
+                if strike==3:
+                    print('> You win')
+                    break
+            game+=1
