91b657a5ff2d563d7cd54d36fe745eb13762c26e
dydtn8653 <dydtn8653@gmail.com>
2019-12-20 09:49:43 +0900

    game2

--- Up_Down.py
+++ Up_Down.py
@@ -1,23 +1,25 @@
 import random
+def GAMEUPDOWN():
+	# random "UP DOWN" game
 
-# random "UP DOWN" game
+	rn = random.randrange(1, 101, 1)
+	num = -1
 
-rn = random.randrange(1, 101, 1)
-num = -1
+	t_cnt = 0 # Try
+	
+	print("1~100 Number Up & Down Game start !!!")
+	print("---------------------------")
+	while ( rn != num ):
+	
+	    num = int(input("Please enter a number between 1 ~ 100. : "))
+	
+	    if (num > rn):
+	        print("Down")
+	    elif (num < rn):
+	        print("Up")
 
-t_cnt = 0 # Try
-
-print("1~100 Number Up & Down Game start !!!")
-print("---------------------------")
-while ( rn != num ):
-
-    num = int(input("Please enter a number between 1 ~ 100. : "))
-
-    if (num > rn):
-        print("Down")
-    elif (num < rn):
-        print("Up")
-
-    t_cnt += 1
-print("---------------------------")
-print("You got the right answer in", t_cnt, "times.")
+	    t_cnt += 1
+	print("---------------------------")
+	print("You got the right answer in", t_cnt, "times.")
+def game2():
+	GAMEUPDOWN()
