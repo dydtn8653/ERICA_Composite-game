8cd682af00f18d856a006369bfa7fe8722b9313e
dydtn8653 <dydtn8653@gmail.com>
2019-12-20 09:51:07 +0900

    game start

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import Odd_Even
-print("숫자선택")
-while true:
-	a = int(input())
+import Up_Down
+import Number_BaseBall
+print("\n****Welcome to the Composite game.****")
+while True:
+	print("\n    1. Odd and Even game\n    2. Up and Down\n    3. Number BaseBall\n    4. Exit")
+	a = int(input("  Please select the game you want.\n  Game Number: "))
 	if a==1 :
-    	Odd_Even.game1()
-	else:
-		print("error")
+		Odd_Even.game1()
+	elif a==2 :
+		Up_Down.game2()
+	elif a==3 :
+		Number_BaseBall.game3()
+	elif a==4 :
+		print("\n  **Thank you for playing the game.**")
+		break
+	else :
+		print("        **Error**")
+		print("**Please re-enter the number**")
 
